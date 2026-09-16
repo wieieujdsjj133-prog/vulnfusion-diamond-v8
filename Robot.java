1f64a0dab3162a35153830d9bfd8683337842b0e
wieieujdsjj133-prog <wieieujdsjj133@gmail.com>
2026-09-16 22:59:31 +0300

    Create Robot.java

---
+++ Robot.java
@@ -0,0 +1,12 @@
+package com.vulnfusion.diamond;
+public abstract class Robot {
+    protected boolean taskCompleted = false;
+    protected String name;
+    public abstract void execute(String task);
+    public boolean isTaskCompleted() { return taskCompleted; }
+    public String getName() { return name; }
+    public void fixAutomatically() {
+        System.out.println("اصلاح تلقائي لـ " + getName());
+        taskCompleted = true;
+    }
+}
